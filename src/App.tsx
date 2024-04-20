0c2a5731402e91a835935add370cdffeb1887eb0
TomoriComAncora <lucas_fernando@ufms.br>
2024-04-19 22:32:59 -0400

    Feat: Adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,56 @@
 import {useState, useEffect} from 'react'
 
+interface Tarefa {
+  texto: string;
+}
+
 function App() {
+  const [tarefaInput, setTarefaInput] = useState<string>('');
+  const [tarefas, setTarefas] = useState<Tarefa[]>(() => {
+    try {
+      const localStorageData = localStorage.getItem('@todos');
+      if (localStorageData) {
+        return JSON.parse(localStorageData) || [];
+      } else {
+        return [];
+      }
+    } catch (err) {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    const buscarItens = (): void => {
+      const tarefasLocal = localStorage.getItem('@todos');
+
+      if (tarefasLocal) {
+        setTarefas(JSON.parse(tarefasLocal));
+      }
+    };
+
+    buscarItens();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@todos', JSON.stringify(tarefas)); 
+  }, [tarefas]);
+
+  const handleAdicionar = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (tarefaInput === '') {
+      alert('Digite uma tarefa');
+      return;
+    }
+
+    if (editar) {
+      handleAtualizarEdicao();
+      return;
+    }
+
+    setTarefas((prevTarefas) => [...prevTarefas, { texto: tarefaInput }]);
+    setTarefaInput('');
+  };
+
   return (
     <div className="app">
       <h1>ToDo's de Prog Web</h1>
